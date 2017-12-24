3f9c4a782c34969fe2f486e2d56eb7d16f1f4228
Zhi Yang <yantze@126.com>
2017-12-24 11:00:10 +0800

    overwrite old resource

--- mod.sh
+++ mod.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
-# set -eu
+set -eu
 
 WFDIR=$(echo ~/Library/Application\ Support/Google/Chrome/Default/Extensions/koegeopamaoljbmhnfjbclbocehhgmkm/1.14_0)
-echo "Your workflowy path:"
-echo $WFDIR
+# WFDIR=$(echo ~/Library/Application\ Support/Google/Chrome/Profile\ 4/Extensions/koegeopamaoljbmhnfjbclbocehhgmkm/1.14_0)
+echo "Your workflowy path may:"
+echo "$WFDIR"
+
+if [[ ! -d $WFDIR ]]; then
+  echo " Can not find Workflowy extension path."
+  exit 1
+fi
 
 if [[ -s "$WFDIR/custom/custom.css" ]]; then
-  echo "You had append before."
+  echo "=== Notice: ==="
+  echo "You had append before. overwrite custom resource."
 else
-  cp -rf custom "$WFDIR"
   sed -i -e '/\<\/body/r append.txt' "$WFDIR/document_view.html"
 fi
+
+cp -rf custom "$WFDIR"
